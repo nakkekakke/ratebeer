e99eae834eb7520dd55b3c52f008966f853dd32b
nakkekakke <juntusen.niko@gmail.com>
2018-09-09 19:07:18 +0300

    fix attempt #2

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :beers
   resources :breweries
-  root :to => 'index#index'
+  root :to => 'breweries#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
